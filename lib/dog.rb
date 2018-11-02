7e107f98759245aa6dd55b7c76532c701deb2235
Ken Hick <hick.ken@gmail.com>
2018-11-02 09:45:35 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,4 +5,19 @@ def initialize(name, breed)
   @breed = breed
 end
 
+def name=(dog_name)
+  @name = dog_name
+end
+
+def name
+  @name
+end
+
+def breed=(dog_breed = "Mutt")
+  @breed = dog_breed
+end
+
+def breed
+  @breed
+end
 end
